33f5467ebb92e41b9a165573daa956038fe6fce8
Sanathkumar Sunakd <sanathsunkad07@gmail.com>
2021-05-20 21:06:21 +0530

    Update testfloat.js

--- testfloat.js
+++ testfloat.js
@@ -1,3 +1,11 @@
 function displayPopup() {
 	alert("Hello! I am an alert box!!");
+	let divEle = document.createElement('div');
+	divEle.setAttribute('style','display: block;position: fixed;bottom: 23px;right: 15px;z-index: 9; width:350px; height:620px;');
+	divEle.setAttribute('id','container-sec');
+	let closeBtn = document.createElement('button');
+	closeBtn.setAttribute('id','close-btn');
+	closeBtn.innerHTML = "Close";
+	closeBtn.setAttribute('style','color: white;padding: 19px 20px;border: none;cursor: pointer;width: 100%;opacity: 0.8;background-color: red;border-radius:15px;outline:none');
+
 }
